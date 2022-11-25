50293cf868be9a595231be692b18514ec587914e
leticea <nascimento.leticeah@gmail.com>
2022-11-25 17:14:21 -0300

    criando arquivo para configurar o BD

---
+++ admin/pages/database.php
@@ -0,0 +1,26 @@
+<?php
+
+$pages_all = function() 
+{
+    // [busca todas as páginas]
+};
+
+$pages_one = function() 
+{
+    // [busca uma única página]
+};
+
+$pages_create = function() 
+{
+    // [cria uma página]
+};
+
+$pages_edit = function() 
+{
+    // [atualiza uma página]
+};
+
+$pages_delete = function() 
+{
+    // [remove uma página]
+};
--- admin/pages/routes.php
+++ admin/pages/routes.php
@@ -1,17 +1,32 @@
 <?php
 
+include __DIR__ . '/database.php';
+
  if (resolve('/admin/pages')) {
+    $pages = $pages_all();
     render('admin/pages/index', 'admin');
 
 } elseif (resolve('/admin/pages/create')) {
+    if ($_SERVER['REQUEST METHOD'] === 'POST') {
+        $pages_create();
+        header('location: /admin/pages');
+    }
     render('admin/pages/create', 'admin'); 
 
 } elseif (resolve('/admin/pages/(\d)+')) {
     render('admin/pages/view', 'admin');  
 
 } elseif (resolve('/admin/pages/(\d)+/edit')) {
+    if ($_SERVER['REQUEST METHOD'] === 'POST') {
+        $pages_edit();
+        header('location: /admin/pages');
+    }
     render('admin/pages/edit', 'admin'); 
 
 } elseif (resolve('/admin/pages/(\d)+/delete')) {
+    if ($_SERVER['REQUEST METHOD'] === 'POST') {
+        $pages_delete();
+        header('location: /admin/pages');
+    }
     header('location: /admin/pages');
 }
