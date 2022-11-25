13af602a99fc2d047bdb0603cd33dfc19b698201
leticea <nascimento.leticeah@gmail.com>
2022-11-25 11:52:04 -0300

    editando o view

--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -3,20 +3,28 @@
 <div class="row">
     <div class="col-3">
         <dl class="row">
-            <dt class="col-sm-3">Título</dt>
-            <dd class="col-sm-9">Página inicial</dd>
+            <dt class="col-sm-4">Título</dt>
+            <dd class="col-sm-8">Página inicial</dd>
 
-            <dt class="col-sm-3">URL</dt>
-            <dd class="col-sm-9">/ - <a href="/" target="blank">abrir</a></dd>
+            <dt class="col-sm-4">URL</dt>
+            <dd class="col-sm-8">/ - <a href="/" target="blank">abrir</a></dd>
 
-            <dt class="col-sm-3">Criado em</dt>
-            <dd class="col-sm-9">2022-11-11 00:00</dd>
+            <dt class="col-sm-4">Criado em</dt>
+            <dd class="col-sm-8">2022-11-11 00:00</dd>
 
-            <dt class="col-sm-3">Atualizado em</dt>
-            <dd class="col-sm-9">2022-11-11 00:00</dd>
+            <dt class="col-sm-4">Atualizado em</dt>
+            <dd class="col-sm-8">2022-11-11 00:00</dd>
         </dl>
     </div>
-    <div class="col"></div>
+    <div class="col bg-light">
+        <h3>Página inicial</h3>
+        <p>Esta é a página inicial do site</p>
+    </div>
 </div>
+
+<p>
+    <a href="/admin/pages/1/edit" class="btn btn-primary">editar</a>
+    <a href="/admin/pages/1/delete" class="btn btn-danger">remover</a>
+</p>
 
 <a href="/admin/pages" class="btn btn-secondary">Voltar</a>
