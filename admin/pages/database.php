b42db4846f61e7997799c1b757f23998ddfbe13b
leticea <nascimento.leticeah@gmail.com>
2022-11-25 19:16:40 -0300

    criando método para guardar/mostrar a mensagem

--- admin/pages/database.php
+++ admin/pages/database.php
@@ -13,14 +13,19 @@ $pages_one = function($id)
 $pages_create = function() 
 {
     // [cria uma página]
+    flash('Registrado com sucesso', 'success');
 };
 
 $pages_edit = function($id) 
 {
     // [atualiza uma página]
+    flash('Atualizado com sucesso', 'success');
+
 };
 
 $pages_delete = function($id) 
 {
     // [remove uma página]
+    flash('Removido com sucesso', 'success');
+
 };
