c8c8d30c510de2c9d6c37357df06b59bcfe57ba8
leticea <nascimento.leticeah@gmail.com>
2022-11-25 11:34:53 -0300

    editando o view

--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -1,5 +1,22 @@
-<h3>Administração de Páginas</h3>
+<h3 class="mb-5">Administração de Páginas</h3>
 
-//visualização
+<div class="row">
+    <div class="col-3">
+        <dl class="row">
+            <dt class="col-sm-3">Título</dt>
+            <dd class="col-sm-9">Página inicial</dd>
 
-<a href="/admin">Home</a>
+            <dt class="col-sm-3">URL</dt>
+            <dd class="col-sm-9">/ - <a href="/" target="blank">abrir</dd>
+
+            <dt class="col-sm-3">Título</dt>
+            <dd class="col-sm-9">Página inicial</dd>
+
+            <dt class="col-sm-3">Título</dt>
+            <dd class="col-sm-9">Página inicial</dd>
+        </dl>
+    </div>
+    <div class="col"></div>
+</div>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
