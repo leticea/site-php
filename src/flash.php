68eeb9e3ac9c5d4fcdcbf8b8f12bd136b6287ef3
leticea <nascimento.leticeah@gmail.com>
2022-11-25 18:51:51 -0300

    criando método para guardar/mostrar a mensagem

--- src/flash.php
+++ src/flash.php
@@ -9,12 +9,16 @@ function flash($message = null, $type = null)
     } else {
         // [mostra a mensagem]
         $flash = $_SESSION['flash'] ?? [];
+        
         if (!count($flash)) {
             return;
         }
 
+        // [AJAX - nesse caso, é um template sem conteúdo]
         foreach ($flash as $key => $message) {
-            
+
+            render('flash', 'ajax', $message);
+            unset($_SESSION['flash'][$key]);
         }
     }
 }
--- system.php
+++ system.php
@@ -7,6 +7,7 @@ require __DIR__ . '/src/error_handler.php';
 require __DIR__ . '/src/resolve.php';
 require __DIR__ . '/src/render.php';
 require __DIR__ . '/src/connection.php';
+require __DIR__ . '/src/flash.php';
 
 if (resolve('/admin/?(.*)')) {
     require __DIR__ . '/admin/routes.php';
