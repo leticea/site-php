888ceb7776a47092234163ce6f15be126e7c7288
leticea <nascimento.leticeah@gmail.com>
2022-11-28 18:28:04 -0300

    inserindo o pnotify

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -7,6 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="/resources/trix/trix.css">
+    <link rel="stylesheet" href="/resources/pnotify/pnotify.custom.min.css">
     <link rel="stylesheet" href="/css/style.css">
 
     <title>Painel Administrativo</title>
@@ -42,6 +43,7 @@
     </div>
     <script src="/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="/resources/trix/trix.js"></script>
+    <script src="/resources/pnotify/pnotify.custom.min.js"></script>
 
     <script>
         <?php flash(); ?>
