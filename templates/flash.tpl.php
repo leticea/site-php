1986955e83a357b729380141e407d3aacfdccd02
leticea <nascimento.leticeah@gmail.com>
2022-11-28 18:56:59 -0300

    inserindo o pnotify

--- templates/flash.tpl.php
+++ templates/flash.tpl.php
@@ -1,1 +1,6 @@
-alert("<?php echo $data['type']; ?>" + "-" + "<?php echo $data['message']; ?>")
+
+PNotify.success({
+  title: 'Success!',
+  text: '<?php echo $data['message']; ?>'
+  type: '<?php echo $data['type']; ?>'
+});
