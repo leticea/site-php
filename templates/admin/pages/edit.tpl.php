aee85b21ba3b3ba577103d53110f59c2af270a80
leticea <nascimento.leticeah@gmail.com>
2022-12-08 11:20:07 -0300

    criando o campo editar

--- templates/admin/pages/edit.tpl.php
+++ templates/admin/pages/edit.tpl.php
@@ -18,12 +18,18 @@
             <div class="input-group-prepend">
                 <span class="input-group-text">/</span>
             </div>
-            <input name="url" id="pagesUrl" type="text" class="form-control" placeholder="URL amigável, deixe em branco para informar a página inicial..." >
+            <input name="url" 
+                id="pagesUrl" 
+                type="text" 
+                class="form-control" 
+                placeholder="URL amigável, deixe em branco para informar a página inicial..." 
+                value="<?php echo $data['page']['url']; ?>"
+            />
         </div>
     </div>
     <br>
     <div class="form-group">
-        <input id="pagesBody" type="hidden" name="body" value="<h3>Página inicial</h3><p>Esta é a página inicial do site</p>">
+        <input id="pagesBody" type="hidden" name="body" value="<?php echo $data['page']['body']; ?>" />
         <trix-editor input="pagesBody"></trix-editor>
     </div>
 
