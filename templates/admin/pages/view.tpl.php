4e5653717b28edd39647fe474f7dc23a2123763e
leticea <nascimento.leticeah@gmail.com>
2022-12-08 11:06:41 -0300

    editando a url

--- templates/admin/pages/view.tpl.php
+++ templates/admin/pages/view.tpl.php
@@ -7,24 +7,23 @@
             <dd class="col-sm-8"><?php echo $data['page']['title']; ?></dd>
 
             <dt class="col-sm-4">URL</dt>
-            <dd class="col-sm-8">/ - <a href="/" target="blank">abrir</a></dd>
+            <dd class="col-sm-8">/<?php echo $data['page']['url']; ?> - <a href="/<?php echo $data['page']['url']; ?>" target="blank">abrir</a></dd>
 
             <dt class="col-sm-4">Criado em</dt>
-            <dd class="col-sm-8">2022-11-11 00:00</dd>
+            <dd class="col-sm-8"><?php echo $data['page']['created']; ?></dd>
 
             <dt class="col-sm-4">Atualizado em</dt>
-            <dd class="col-sm-8">2022-11-11 00:00</dd>
+            <dd class="col-sm-8"><?php echo $data['page']['updated']; ?></dd>
         </dl>
     </div>
     <div class="col bg-light">
-        <h3>Página inicial</h3>
-        <p>Esta é a página inicial do site</p>
+        <?php echo $data['page']['body']; ?>
     </div>
 </div>
 
 <p>
-    <a href="/admin/pages/1/edit" class="btn btn-primary">editar</a>
-    <a href="/admin/pages/1/delete" class="btn btn-danger confirm">remover</a>
+    <a href="/admin/pages/<?php echo $data['page']['id']; ?>/edit" class="btn btn-primary">editar</a>
+    <a href="/admin/pages/<?php echo $data['page']['id']; ?>/delete" class="btn btn-danger confirm">remover</a>
 </p>
 
 <a href="/admin/pages" class="btn btn-secondary">Voltar</a>
