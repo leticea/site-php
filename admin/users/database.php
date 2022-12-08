b35cbede4bc29b7ca9602fead45cf92af47618db
leticea <nascimento.leticeah@gmail.com>
2022-12-08 19:31:27 -0300

    inserindo dados no database dos users

--- admin/users/database.php
+++ admin/users/database.php
@@ -1,5 +1,20 @@
 <?php
 
+function users_get_data ($redirectOnError) {
+
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+
+    if (is_null($email)) {
+
+        flash('Informe o campo email', 'error');
+        header('location: ' . $redirectOnError);
+        die();
+    }
+
+    return compact('email', 'password');
+}
+
 $users_all = function() use ($conn)
 {
     $result = $conn->query('SELECT * FROM users');
@@ -8,7 +23,7 @@ $users_all = function() use ($conn)
 
 $users_view = function() use ($conn)
 {
-
+    $data = users_get_data('/admin/users/create');
 };
 
 $users_create = function() use ($conn)
