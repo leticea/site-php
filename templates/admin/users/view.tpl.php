b22a39c24f1c323aab02899d7c7755101a929e85
leticea <nascimento.leticeah@gmail.com>
2022-12-08 18:13:08 -0300

    criando os templates dos users

--- templates/admin/users/view.tpl.php
+++ templates/admin/users/view.tpl.php
@@ -1,1 +1,19 @@
-// detalhes de users
+<h3 class="mb-5">Administração de usuários</h3>
+
+<dl class="rol">
+    <dt class="col-sm-3">Email</dt>
+    <dd class="col-sm-9">leticea@leticea.com</dd>
+
+    <dt class="col-sm-3">Criado em</dt>
+    <dd class="col-sm-9">2022-12-08 18:10</dd>
+
+    <dt class="col-sm-3">Atualizado em</dt>
+    <dd class="col-sm-9">2022-12-08 18:10</dd>
+</dl>
+
+<p>
+    <a href="/admin/users/1/edit" class="btn btn-primary">editar</a>
+    <a href="/admin/users/1/delete" class="btn btn-danger confirm">remover</a>
+</p>
+
+<a href="/admin/users" class="btn btn-secondary">Voltar</a>
