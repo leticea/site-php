dc11448fa0a90716327181021b5f86b7dc19f5cc
leticea <nascimento.leticeah@gmail.com>
2022-12-10 10:37:51 -0300

    editando a página

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/resources/trix/trix.css">
     <link rel="stylesheet" href="/resources/pnotify/pnotify.custom.min.css">
     <link rel="stylesheet" href="/css/style.css">
-
+    <script src="https://kit.fontawesome.com/35b38b060a.js" crossorigin="anonymous"></script>
     <title>Painel Administrativo</title>
 </head>
 <body class="d-flex flex-column">
     <div id="header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a href="" class="navbar-brand">Admin</a>
+            <a href="/admin" class="navbar-brand">Admin</a>
             <span class="navbar-text">
                 Painel Administrativo
             </span>
@@ -28,10 +28,10 @@
                         <span href="" class="nav-link text-white-50"><small>MENU</small></span>
                     </li>
                     <li class="nav-item">
-                        <a href="/admin/pages" class="nav-link<?php if (resolve('/admin/pages.*')): ?> active<?php endif; ?>">Páginas</a>
+                        <a href="/admin/pages" class="nav-link<?php if (resolve('/admin/pages.*')): ?> active<?php endif; ?>"><i class="fa-solid fa-file-lines"></i> Páginas</a>
                     </li>
                     <li class="nav-item">                        
-                        <a href="/admin/users" class="nav-link">Usuários</a>
+                        <a href="/admin/users" class="nav-link <?php if (resolve('/admin/users.*')): ?> active<?php endif; ?>"><i class="fa-solid fa-user"></i> Usuários</a>
                     </li>                    
                 </ul>
             </div>
@@ -46,8 +46,8 @@
     <script src="/js/bootstrap.min.js"></script>
     <script src="/resources/trix/trix.js"></script>
     <script src="/resources/pnotify/pnotify.custom.min.js"></script>
-    <script>
 
+    <script>
         document.addEventListener('trix-attachment-add', function() {
 
             const attachment = event.attachment;
